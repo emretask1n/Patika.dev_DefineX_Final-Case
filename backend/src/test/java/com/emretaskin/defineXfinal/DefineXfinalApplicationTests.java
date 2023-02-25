227f1191a52c90df4de9fbea4b48c546856a903c
Emre Taşkın <emretaskn57@gmail.com>
2023-02-25 18:09:37 +0300

    Initial Commit

---
+++ backend/src/test/java/com/emretaskin/defineXfinal/DefineXfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emretaskin.defineXfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefineXfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
